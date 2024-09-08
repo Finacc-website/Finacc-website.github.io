15a1d3d6e079cf1d0026cc447806fb2041bc9636
Saskithy <kjimenezmartinez7@gmail.com>
2024-09-07 21:57:36 -0600

    Proyect website

---
+++ Simulators/Simulator4/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.modify-btn').addEventListener('click', function() {
+    alert('Modificar los costos y gastos.');
+});
+ 
+document.querySelector('.reset-btn').addEventListener('click', function() {
+    alert('Restablecer los valores a 0.');
+});
+ 
+document.querySelector('.next-btn').addEventListener('click', function() {
+    alert('Pasar al siguiente paso.');
+});
